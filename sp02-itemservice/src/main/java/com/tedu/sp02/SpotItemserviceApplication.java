b106e64eb85b551422ccd40b237c3a19b95aad9b
tbz <tbz@DESKTOP-QEUCUIC>
2019-08-02 10:44:03 +0800

    gh

---
+++ sp02-itemservice/src/main/java/com/tedu/sp02/SpotItemserviceApplication.java
@@ -0,0 +1,13 @@
+package com.tedu.sp02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotItemserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotItemserviceApplication.class, args);
+	}
+
+}
